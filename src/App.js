07383701319fdd61850aeddb1623445e77cf757d
Mackenzie Storey <mackenzie.storey@3dplaneta.com>
2022-10-20 15:53:29 -0300

    Added JSON for options, removed unused files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import { ChakraProvider, Container, theme, Box } from '@chakra-ui/react';
-import { Navbar } from './components/Navbar'
+import React, { useState } from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Container p="0" maxW="1280px">
         <Navbar />
-      </Container>
     </ChakraProvider>
   );
 }
