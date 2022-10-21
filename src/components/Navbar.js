b27de8e1babdea5e58645f50a7c52560415f0c78
Mackenzie Storey <mackenzie.storey@3dplaneta.com>
2022-10-21 10:33:15 -0300

    Separated navbar css, added comments
    - Moved most of the navbar CSS to the self-contained file to separate logic.
    
    - Added code comments to explain decisions

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ import logo from '../logo.svg';
 const Navbar = props => {
     const mobileNav = useDisclosure();
     const isDesktop = useBreakpointValue({ base: false, md: true });
-    const { navbar } = useStyle();
+    const { navbar, containerBackground, logoMain, menuButton, mainContainer } = useStyle();
     const navButtons = () => {
         if (isDesktop) {
             mobileNav.isOpen = false;
@@ -41,18 +41,17 @@ const Navbar = props => {
     }
 
     return (
-        <Box minH="100vh" maxW="100vw" bg="#E8E9EB">
+        <Box css={mainContainer}>
             <Center>
-                <Flex css={navbar.desktop} shadow="xl" >
-                    <Image pl="10px" bg="#E0DFD5" rounded="lg" src={logo} pr="0.5rem" alt="logo ipsum" />
+                <Flex css={navbar} shadow="xl" >
+                    <Image css={logoMain} src={logo} alt="logo ipsum" />
                     {isDesktop ? navButtons() : (
                         <>
                             <Spacer />
                             <IconButton
                                 display={!mobileNav.isOpen ? "flex" : "none"}
                                 aria-label="Open menu"
-                                fontSize="20px"
-                                color="#5924ce"
+                                css={menuButton}
                                 variant="ghost"
                                 icon={<AiOutlineMenu />}
                                 onClick={mobileNav.onOpen}
@@ -60,6 +59,10 @@ const Navbar = props => {
                         </>
                     )}
                 </Flex>
+                {/* I would have liked to have the mobile drawer in its own component 
+                file. You might see the file in the commit history. I ran into some troubles 
+                getting it to work and was running out of time, so I 
+                left it as is. This would definitely be a TODO */}
                 <Drawer
                     isOpen={mobileNav.isOpen}
                     placement='top'
@@ -68,13 +71,13 @@ const Navbar = props => {
                 >
                     <DrawerOverlay />
                     <DrawerContent>
-                        <DrawerCloseButton color="#5924ce" />
-                        <DrawerBody bg="#E0DFD5">
+                        <DrawerCloseButton css={menuButton} />
+                        <DrawerBody css={containerBackground}>
                             <Flex direction="column" alignItems="center">
                                 {navButtons()}
                             </Flex>
                         </DrawerBody>
-                        <DrawerFooter bg="#E0DFD5">
+                        <DrawerFooter css={containerBackground}>
                             <Image flex="1" rounded="lg" src={logo} alt="logo ipsum" />
                         </DrawerFooter>
                     </DrawerContent>
--- src/components/NavbarStyles.js
+++ src/components/NavbarStyles.js
@@ -1,22 +1,35 @@
 const buildStyle = () => {
     return {
         navbar: {
-            desktop: {
-                alignItems: "center",
-                paddingLeft: "18px",
-                paddingRight: "18px",
-                paddingBottom: "10px",
-                paddingTop: "10px",
-                maxWidth: "1280px",
-                borderBottomLeftRadius: "0.375rem",
-                borderBottomRightRadius: "0.375rem",
-                borderWidth: "0px 1px 1px 1px",
-                borderColor: "#5924ce",
-                gap: "5px",
-                background: "#E0DFD5",
-                width: "100%"
-            }
-
+            alignItems: "center",
+            paddingLeft: "18px",
+            paddingRight: "18px",
+            paddingBottom: "10px",
+            paddingTop: "10px",
+            maxWidth: "1280px",
+            borderBottomLeftRadius: "0.375rem",
+            borderBottomRightRadius: "0.375rem",
+            borderWidth: "0px 1px 1px 1px",
+            borderColor: "#5924ce",
+            gap: "5px",
+            background: "#E0DFD5",
+            width: "100%"
+        },
+        mainContainer: {
+            minHeight: "100vh",
+            minWidth: "100vw",
+            background: "#E8E9EB"
+        },
+        containerBackground: {
+            background: "#E0DFD5"
+        },
+        logoMain: {
+            paddingLeft: "10px",
+            paddingRight: "0.5rem"
+        },
+        menuButton: {
+            fontSize: "20px",
+            color: "#5924ce"
         }
     }
 }
@@ -26,6 +39,18 @@ export const useStyle = () => {
     return {
         navbar: {
             ...styles.navbar
+        },
+        mainContainer: {
+            ...styles.mainContainer
+        },
+        containerBackground: {
+            ...styles.containerBackground
+        },
+        logoMain: {
+            ...styles.logoMain
+        },
+        menuButton: {
+            ...styles.menuButton
         }
     }
 }
